a8be54ed7d4fdff671bee97c88efcc8b921fb781
Bhargav-2808 <bhargavvalani01@gmail.com>
2021-12-13 14:08:15 +0530

    nav updated

--- mernapp/src/Adduser.js
+++ mernapp/src/Adduser.js
@@ -1,35 +1,48 @@
+import { useState } from "react";
 import react from "react";
 
 const Adduser = () => {
+    const [name, setname] = useState('');
+    const [age, setage] = useState('');
+    const [email, setemail] = useState('');
+    const [mobile, setmobile] = useState('');
+
+
+    const submit = (e) =>{
+        e.preventDefault();
+    }
+
   return (
     <>
-      <form>
+      <form onSubmit={submit}>
         <div>
           <table>
+              <tbody>
             <tr>
               <td>Name</td>
               <td>
-                <input type="text" />
+                <input type="text" value={name} onChange={(e) => setname(e.target.value)}/>
               </td>
             </tr>
             <tr>
               <td>age</td>
               <td>
-                <input type="text" />
+                <input type="text" value={age} onChange={(e) => setage(e.target.value)}/>
               </td>
             </tr>
             <tr>
               <td>email</td>
               <td>
-                <input type="text" />
+                <input type="text" value={email} onChange={(e) => setemail(e.target.value)}/>
               </td>
             </tr>
             <tr>
               <td>mobile</td>
               <td>
-                <input type="text" />
+                <input type="text" value={mobile} onChange={(e) => setmobile(e.target.value)}/>
               </td>
             </tr>
+            </tbody>
           </table>
         </div>
       </form>
--- mernapp/src/Crud.js
+++ mernapp/src/Crud.js
@@ -1,6 +1,7 @@
 const Crud = () =>{
     return(
         <>
+            
            
         </>
     );
