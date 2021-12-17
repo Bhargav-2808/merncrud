c673e774710025bc09185af74711462c63ea223c
Bhargav-2808 <bhargavvalani01@gmail.com>
2021-12-17 20:04:11 +0530

    adduser updated

--- mernapp/src/Adduser.js
+++ mernapp/src/Adduser.js
@@ -1,34 +1,67 @@
-import react , { useState } from "react";
-import Links from './Links';
+import {useForm} from 'react-hook-form'
+import react, { useState } from "react";
+import Links from "./Links";
+import {Form, Button, Container} from 'react-bootstrap';
 import { addusers } from "./api";
 
+// const initialvalue = {
+//   name: "",
+//   age: "",
+//   email: "",
+//   mobile: "",
+// };
 
 
-const initialvalue = {
-  name: "",
-  age: "",
-  email: "",
-  mobile: "",
-};
-
-  const Adduser = () => {
-  const [user, setUser] = useState(initialvalue);
-  const {name, age, email, mobile} = user;
+const Adduser = () => {
   
+  
+  const {register , handleSubmit} = useForm();
+  const onSubmit = async data =>{
+    await addusers(data);
+    //console.log(data);
+  }
 
-  const commonFormSet = (e) => {
-    setUser({ ...user, [e.target.name]: e.target.value });
-  };
+  // const [user, setUser] = useState(initialvalue);
+  // const { name, age, email, mobile } = user;
+  
+  // const commonFormSet = (e) => {
+  //   setUser({ ...user, [e.target.name]: e.target.value });
+  // };
 
-  const onValueChange = async () => {
-    await addusers(user);
-   
-  };
+  // const onValueChange = async () => {
+  //   await addusers(user);
+  // };
 
   return (
     <>
-      <Links/>
-      <form>
+      <Links />
+      
+      <Container className="mt-5">
+      <Form onSubmit={handleSubmit(onSubmit)}>
+        <Form.Group className="mb-3" >
+          <Form.Label>Full name</Form.Label>
+          <Form.Control type="text" placeholder="Enter Full-Name" name="name" {...register("name")}/>
+        </Form.Group>
+        <Form.Group className="mb-3" >
+          <Form.Label>Age</Form.Label>
+          <Form.Control type="text" placeholder="Enter Age" name="age"  {...register("age")}/>
+        </Form.Group>
+        <Form.Group className="mb-3" >
+          <Form.Label>Email</Form.Label>
+          <Form.Control type="email" placeholder="Enter email" name="email"  {...register("email")}/>
+        </Form.Group>
+        <Form.Group className="mb-3" >
+          <Form.Label>Mobile Number</Form.Label>
+          <Form.Control type="number" placeholder="Enter Mobile-Number" name="mobile"  {...register("mobile")}/>
+        </Form.Group>
+        
+        
+        <Button variant="primary" type="submit">
+          Submit
+        </Button>
+      </Form>
+      </Container>
+      {/* <form>
         <div>
           <table>
             <tbody>
@@ -76,14 +109,13 @@ const initialvalue = {
                   />
                 </td>
               </tr>
-             
             </tbody>
           </table>
         </div>
       </form>
       <button type="submit" onClick={() => onValueChange()}>
-                Submit
-      </button>
+        Submit
+      </button> */}
     </>
   );
 };
