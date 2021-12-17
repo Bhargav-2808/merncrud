2ffe1507c044a2c1b563eb4d3e919cd7ee1f8c18
Bhargav-2808 <bhargavvalani01@gmail.com>
2021-12-17 20:13:37 +0530

    Edit user Updated

--- mernapp/src/Adduser.js
+++ mernapp/src/Adduser.js
@@ -4,12 +4,7 @@ import Links from "./Links";
 import {Form, Button, Container} from 'react-bootstrap';
 import { addusers } from "./api";
 
-// const initialvalue = {
-//   name: "",
-//   age: "",
-//   email: "",
-//   mobile: "",
-// };
+
 
 
 const Adduser = () => {
@@ -21,17 +16,7 @@ const Adduser = () => {
     //console.log(data);
   }
 
-  // const [user, setUser] = useState(initialvalue);
-  // const { name, age, email, mobile } = user;
   
-  // const commonFormSet = (e) => {
-  //   setUser({ ...user, [e.target.name]: e.target.value });
-  // };
-
-  // const onValueChange = async () => {
-  //   await addusers(user);
-  // };
-
   return (
     <>
       <Links />
@@ -61,61 +46,7 @@ const Adduser = () => {
         </Button>
       </Form>
       </Container>
-      {/* <form>
-        <div>
-          <table>
-            <tbody>
-              <tr>
-                <td>Name</td>
-                <td>
-                  <input
-                    type="text"
-                    onChange={(e) => commonFormSet(e)}
-                    value={name}
-                    name="name"
-                  />
-                </td>
-              </tr>
-              <tr>
-                <td>age</td>
-                <td>
-                  <input
-                    type="text"
-                    onChange={(e) => commonFormSet(e)}
-                    value={age}
-                    name="age"
-                  />
-                </td>
-              </tr>
-              <tr>
-                <td>email</td>
-                <td>
-                  <input
-                    type="text"
-                    onChange={(e) => commonFormSet(e)}
-                    value={email}
-                    name="email"
-                  />
-                </td>
-              </tr>
-              <tr>
-                <td>mobile</td>
-                <td>
-                  <input
-                    type="text"
-                    onChange={(e) => commonFormSet(e)}
-                    value={mobile}
-                    name="mobile"
-                  />
-                </td>
-              </tr>
-            </tbody>
-          </table>
-        </div>
-      </form>
-      <button type="submit" onClick={() => onValueChange()}>
-        Submit
-      </button> */}
+      
     </>
   );
 };
--- mernapp/src/Edituser.js
+++ mernapp/src/Edituser.js
@@ -1,105 +1,75 @@
 import react, { useState, useEffect } from "react";
-import Links from './Links';
-import { addusers, editUser } from "./api";
+import Links from "./Links";
+import {  editUser } from "./api";
+import {Form, Container, Button} from 'react-bootstrap';
 import { useParams } from "react-router-dom";
+import { useForm } from "react-hook-form";
 
-
-
-const initialvalue = {
-    name: "",
-    age: "",
-    email: "",
-    mobile: "",
-};
+// const initialvalue = {
+//   name: "",
+//   age: "",
+//   email: "",
+//   mobile: "",
+// };
 
 const Edituser = () => {
-    const [user, setUser] = useState(initialvalue);
-    const { name, age, email, mobile } = user;
-    const {id} = useParams();
+  const { id } = useParams();
+  const { register, handleSubmit } = useForm();
+  const onSubmit = async (data) => {
+    await editUser(id, data);
+    //console.log(data);
+  };
+ 
 
+  return (
+    <>
+      <Links />
 
-    // useEffect(() => {
-    //     loadUserDetails();
-    // }, []);
+      <Container className="mt-5">
+        <Form onSubmit={handleSubmit(onSubmit)}>
+          <Form.Group className="mb-3">
+            <Form.Label>Full name</Form.Label>
+            <Form.Control
+              type="text"
+              placeholder="Enter Full-Name"
+              name="name"
+              {...register("name")}
+            />
+          </Form.Group>
+          <Form.Group className="mb-3">
+            <Form.Label>Age</Form.Label>
+            <Form.Control
+              type="text"
+              placeholder="Enter Age"
+              name="age"
+              {...register("age")}
+            />
+          </Form.Group>
+          <Form.Group className="mb-3">
+            <Form.Label>Email</Form.Label>
+            <Form.Control
+              type="email"
+              placeholder="Enter email"
+              name="email"
+              {...register("email")}
+            />
+          </Form.Group>
+          <Form.Group className="mb-3">
+            <Form.Label>Mobile Number</Form.Label>
+            <Form.Control
+              type="number"
+              placeholder="Enter Mobile-Number"
+              name="mobile"
+              {...register("mobile")}
+            />
+          </Form.Group>
 
-    // const loadUserDetails = async () => {
-    //     const response = await addusers(id);
-    //     setUser(response.data);
-    // }
-
-    const commonFormSet = (e) => {
-        setUser({ ...user, [e.target.name]: e.target.value });
-    };
-
-    const onValueChange = async () => {
-        const response =   await editUser(id, user);
-
-    };
-  
-
-
-
-
-    return (
-        <>
-             <Links />
-            <form>
-                <div>
-                    <table>
-                        <tbody>
-                            <tr>
-                                <td>Name</td>
-                                <td>
-                                    <input
-                                        type="text"
-                                        onChange={(e) => commonFormSet(e)}
-                                        value={name}
-                                        name="name"
-                                    />
-                                </td>
-                            </tr>
-                            <tr>
-                                <td>age</td>
-                                <td>
-                                    <input
-                                        type="text"
-                                        onChange={(e) => commonFormSet(e)}
-                                        value={age}
-                                        name="age"
-                                    />
-                                </td>
-                            </tr>
-                            <tr>
-                                <td>email</td>
-                                <td>
-                                    <input
-                                        type="text"
-                                        onChange={(e) => commonFormSet(e)}
-                                        value={email}
-                                        name="email"
-                                    />
-                                </td>
-                            </tr>
-                            <tr>
-                                <td>mobile</td>
-                                <td>
-                                    <input
-                                        type="text"
-                                        onChange={(e) => commonFormSet(e)}
-                                        value={mobile}
-                                        name="mobile"
-                                    />
-                                </td>
-                            </tr>
-
-                        </tbody>
-                    </table>
-                </div>
-            </form>
-            <button type="submit" onClick={() => onValueChange()}>
-                Submit
-            </button>
-        </>
-    );
+          <Button variant="primary" type="submit">
+            Submit
+          </Button>
+        </Form>
+      </Container>
+    </>
+  );
 };
 export default Edituser;
